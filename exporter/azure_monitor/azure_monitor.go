dcf53253cf02d18cefada116c03ec93ad9c693ce
Christian Coello <t-chcoel@microsoft.com>
2019-06-18 15:42:02 -0700

    Basic Layout of Trace Exporter. Not fully functional.

--- exporter/azure_monitor/azure_monitor.go
+++ exporter/azure_monitor/azure_monitor.go
@@ -2,9 +2,47 @@ package azure_monitor
 
 import (
 	"fmt"
+	"errors"
+	"go.opencensus.io/exporter/azure_monitor/common"
+	"go.opencensus.io/trace"
 )
 
-func NewExporter() (int) {
-	fmt.Printf("hi man")
-	return 1
+
+type AzureTraceExporter struct {
+	projectID          string
+	InstrumentationKey string
+	options            common.Options
+}
+
+func NewAzureTraceExporter(o common.Options) (*AzureTraceExporter, error) {
+	if o.InstrumentationKey == "" {
+		return nil, errors.New("missing Instrumentation Key for Azure Exporter")
+	}
+	e := &AzureTraceExporter {
+		projectID:          "abcdefghijk",
+		InstrumentationKey: o.InstrumentationKey,
+		options:            o,
+	}
+	return e, nil
+}
+
+var _ trace.Exporter = (*AzureTraceExporter)(nil)
+
+//Export SpanData to Azure Monitor
+// The () before the function name means it is a function of AzureTraceExporter
+func (e *AzureTraceExporter) ExportSpan(sd *trace.SpanData) {
+	baseObj := common.BaseObject {IKey : e.options.InstrumentationKey}
+	envelope := common.Envelope {
+		BaseObject: baseObj,
+		//StartTime:  		sd.StartTime,
+	}
+	envelope.Name = "Microsoft.ApplicationInsights.RemoteDependency"
+
+	transporter := common.Transporter{ 
+		Envel: envelope,
+	}
+	transporter.Transmit(&e.options, &envelope)
+
+	fmt.Printf("Name: %s\nTraceID: %x\nSpanID: %x\nParentSpanID: %x\nStartTime: %s\nEndTime: %s\nAnnotations: %+v\n\n",
+		sd.Name, sd.TraceID, sd.SpanID, sd.ParentSpanID, sd.StartTime, sd.EndTime, sd.Annotations)
 }
--- exporter/azure_monitor/example/main.go
+++ exporter/azure_monitor/example/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"context" // similar to log
+	"log"
 	"go.opencensus.io/exporter/azure_monitor"
+	"go.opencensus.io/exporter/azure_monitor/common"
+	"go.opencensus.io/trace"
 )
 
+
 func main() {
-	exporter := azure_monitor.NewExporter()
-	exporter = exporter
+
+	ctx := context.Background()
+
+	exporter, err := azure_monitor.NewAzureTraceExporter(common.Options{
+		InstrumentationKey: "111111111111111111", // add your InstrumentationKey
+		EndPoint: 			"https://dc.services.visualstudio.com/v2/track",
+		TimeOut: 			10.0,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	trace.RegisterExporter(exporter)
+
+	ctx, span := trace.StartSpan(ctx, "/foo") // This calls the function ExportSpan written in azure_monitor.go 
+	span.End()
+
 }
+
+
+
